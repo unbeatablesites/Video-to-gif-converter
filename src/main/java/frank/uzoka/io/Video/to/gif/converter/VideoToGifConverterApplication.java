932439a17da117e6415d96331ab750a5be807514
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-13 18:46:24 -0400

    Adding dependencies

---
+++ src/main/java/frank/uzoka/io/Video/to/gif/converter/VideoToGifConverterApplication.java
@@ -0,0 +1,13 @@
+package frank.uzoka.io.Video.to.gif.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoToGifConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoToGifConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/frank/uzoka/io/Video/to/gif/converter/VideoToGifConverterApplicationTests.java
@@ -0,0 +1,16 @@
+package frank.uzoka.io.Video.to.gif.converter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoToGifConverterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
